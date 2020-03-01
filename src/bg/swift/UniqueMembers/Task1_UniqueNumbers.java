7558e83f7816b005b53a512fb3d363279c221b94
cholakv <hristo_ixa@abv.bg>
2020-03-01 17:50:26 +0200

    Task 1 completed

---
+++ src/bg/swift/UniqueMembers/Task1_UniqueNumbers.java
@@ -0,0 +1,23 @@
+package bg.swift.UniqueMembers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1_UniqueNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+        int[] array = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+
+        }
+        int[] sorted = Arrays.stream(array).sorted().distinct().toArray();
+        for (int element : sorted) {
+            System.out.println(element);
+        }
+
+
+    }
+}
